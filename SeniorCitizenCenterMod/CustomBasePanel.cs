6e2aaf1539610aa2702a00ca11145737521c5310
BoostHungry <kyle.studd@gmail.com>
2015-11-23 22:53:16 -0500

    CopyPasta: Updating Logging Messages of CustomBasePanel

--- SeniorCitizenCenterMod/CustomBasePanel.cs
+++ SeniorCitizenCenterMod/CustomBasePanel.cs
@@ -11,7 +11,7 @@ namespace SeniorCitizenCenterMod {
         }
 
         public override void RefreshPanel() {
-            Logger.logInfo(PanelHelper.LOG_CUSTOM_PANELS, "CustomHealthcarePanel.RefreshPanel");
+            Logger.logInfo(PanelHelper.LOG_CUSTOM_PANELS, "CustomBasePanel.RefreshPanel");
 
             // Refresh the panel
             base.RefreshPanel();
@@ -23,19 +23,19 @@ namespace SeniorCitizenCenterMod {
                 }
             }
 
-            Logger.logInfo(PanelHelper.LOG_CUSTOM_PANELS, "CustomHealthcarePanel.RefreshPanel Done");
+            Logger.logInfo(PanelHelper.LOG_CUSTOM_PANELS, "CustomBasePanel.RefreshPanel Done");
         }
 
         public void removeAllChildren() {
             foreach (UIComponent comp in this.childComponents) {
-                Logger.logInfo(PanelHelper.LOG_CUSTOM_PANELS, "CustomHealthcarePanel.removeAllChildren -- Removing Child Comp: {0}", comp);
+                Logger.logInfo(PanelHelper.LOG_CUSTOM_PANELS, "CustomBasePanel.removeAllChildren -- Removing Child Comp: {0}", comp);
                 this.GetComponentInChildren<UIScrollablePanel>().RemoveUIComponent(comp);
                 Destroy(comp.gameObject);
             }
         }
 
         protected override void OnButtonClicked(UIComponent comp) {
-            Logger.logInfo(PanelHelper.LOG_CUSTOM_PANELS, "CustomHealthcarePanel.OnButtonClicked -- Component Clicked: {0}", comp);
+            Logger.logInfo(PanelHelper.LOG_CUSTOM_PANELS, "CustomBasePanel.OnButtonClicked -- Component Clicked: {0}", comp);
             BuildingInfo buildingInfo = comp.objectUserData as BuildingInfo;
             if (!((Object) buildingInfo != (Object) null))
                 return;
